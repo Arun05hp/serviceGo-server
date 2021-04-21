52f1bb050fe57a1881f806d6e5fb89b8fe76a144
Arun Kumar <kumar05.962013@gmail.com>
2021-04-21 09:19:35 +0530

    deal api

--- src/routes/feedbackRoutes.js
+++ src/routes/feedbackRoutes.js
@@ -1,37 +1,37 @@
-const { feedback } = require("../../models");
+const { Jobs } = require("../../models");
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
-const requireAuth = require("../middlewares/auth");
-const jwt = require("jsonwebtoken");
-
-const https = require("https");
 
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
-  const { userid, rating, comment, recommend } = req.body;
+  const { workerid, userid, rating, comment, recommend } = req.body;
 
   try {
-    const feedbackData = await feedback.findOne({
+    const feedbackData = await Jobs.findOne({
       where: {
-        id: userid,
+        userid: workerid,
       },
     });
 
-    if (!feedbackData)
-      return res.status(400).json({ message: "User not found" });
+    if (!Jobs) return res.status(400).json({ message: "User not found" });
 
-    let feed = JSON.parse(feedbackData.dataValues.feedback);
-    feed = JSON.stringify([...req.body]);
-    Object.assign(feedbackData, {
-      feed,
-    });
-    await user.save();
+    if (feedbackData.dataValues.feedback) {
+      Object.assign(feedbackData, {
+        feedback: [
+          ...feedbackData.dataValues.feedback,
+          { userid, rating, comment, recommend },
+        ],
+      });
+    } else {
+      Object.assign(feedbackData, {
+        feedback: [{ userid, rating, comment, recommend }],
+      });
+    }
+
+    await feedbackData.save();
 
     res.json({
       message: "Success",
-      userDetails: user.dataValues,
     });
   } catch (error) {
     console.log(error);
